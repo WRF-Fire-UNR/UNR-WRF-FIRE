a4129987c2a1da8257b13cd56ffb287417c4f91d
WRF-Fire-UNR <90069633+WRF-Fire-UNR@users.noreply.github.com>
2021-09-10 20:00:53 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'WRF-Fire Wiki'
-copyright = '2021'
+copyright = '2021 '
 author = 'University of Nevada, Reno'
 
 # The full version, including alpha/beta/rc tags
