86bd2b2c45ee7bf563aeeede6a1d7eaae1678cb2
WRF-Fire-UNR <90069633+WRF-Fire-UNR@users.noreply.github.com>
2022-02-19 12:14:19 -0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -48,7 +48,6 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_logo = '3-1.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
